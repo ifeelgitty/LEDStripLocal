ec8a8090e8f1adadd9705c6c0948096ce5f683bf
ifeelgitty <42966018+ifeelgitty@users.noreply.github.com>
2019-06-14 13:08:21 +0200

    Create UDPReceiver.py

---
+++ UDPReceiver.py
@@ -0,0 +1,227 @@
+# Stuff relating UDP connection
+import socket
+# Set IP of the Raspberry Pi
+UDP_IP = "145.90.176.56"
+# Set Port used bz Simulator to send data
+UDP_PORT = 30002
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+# Stuff relating LED Strip
+import time
+import RPi.GPIO as GPIO
+# Import the WS2801 module.
+import Adafruit_WS2801
+import Adafruit_GPIO.SPI as SPI
+# Configure the count of pixels(32 per meter):
+PIXEL_COUNT = 192
+# The radius which  
+LED_DEGREES = 180
+# Alternatively specify a hardware SPI connection on /dev/spidev0.0:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
+
+import math
+"""
+The vehicle class is basically used as a struct to contain data regarding the vehicles
+x = x-position
+y = y-position
+v = velocity
+h = heading
+e_d = distance to ego car
+e_a = angle relative to ego car
+"""
+class Vehicle:
+    def __init__(self, x=0, y=0, v=0, h=0, e_d=0, e_a=0):
+        self.x = x
+        self.y = y
+        self.v = v
+        # Compensating for a bug in the sim, sometimes showing negative degrees
+        if h < 0:
+            self.h = 360 - h
+        else:
+            self.h = h
+        self.e_d = e_d
+        self.e_a = e_a
+    # Function to show all assigned variables of the Vehicle, for debugging
+    def status(self):
+        print("x: " + str(self.x))
+        print("y: " + str(self.y))
+        print("v: " + str(self.v))
+        print("h: " + str(self.h))
+        print("e_d: " + str(self.e_d))
+        print("e_a: " + str(self.e_a))
+    
+#Formatting String into the single Vehicle variables    
+def format_string(s):
+    # Removing Whitespaces
+    s = s.replace(' ', '')
+    # separate into list
+    l = s.split('/')
+    # Create list to hold items
+    vehicles = []
+    # Creating an Object of the Class Vehicle per vehicle.
+    for i in range(5):
+        vehicles.append(Vehicle(
+            float(l[1 + 8*i]),
+            float(l[3 + 8*i]),
+            float(l[5 + 8*i]),
+            float(l[7 + 8*i])
+        ))
+    return(vehicles)
+
+# Transposing the vehicles in relation to ego-vehicle
+def transposing_vehicles(vehicles):
+    
+    # Transposing origin to ego vehicle
+    for i in range(1,5):
+        vehicles[i].x = vehicles[0].x - vehicles[i].x
+        vehicles[i].y = vehicles[0].y - vehicles[i].y
+    vehicles[0].x = 0
+    vehicles[0].y = 0
+    
+    # Rotate matrix, so that ego vehicle's direction is 0
+    rot = -(vehicles[0].h) - 90
+    # 
+    rot_rad = math.radians(rot)
+    for i in range(1,5):
+        x = vehicles[i].x
+        y = vehicles[i].y
+        vehicles[i].x = x*math.cos(rot_rad) - y*math.sin(rot_rad)
+        vehicles[i].y = x*math.sin(rot_rad) + y*math.cos(rot_rad)
+        vehicles[i].h = vehicles[i].h + rot
+    vehicles[0].h = 0
+    return vehicles
+        
+# Computing the distances relative to the ego car
+def dist_to_ego(vehicles):
+    # Just calculate hypotenuse of coordinates(automatically the x/y distances)
+    for i in range(1,5):
+        vehicles[i].e_d = math.sqrt(vehicles[i].x**2 + vehicles[i].y**2)
+    return vehicles
+
+def angle_to_ego(vehicles):
+    for i in range(1,5):
+        vehicles[i].e_a = math.degrees(math.atan2(math.radians(vehicles[i].x), math.radians(vehicles[i].y)))
+        if vehicles[i].e_a < 0:
+            vehicles[i].e_a = 360 + vehicles[i].e_a
+    return vehicles
+
+def led_position(veh, step, f_pixels):
+    if veh.e_a <= LED_DEGREES/2:
+        return round((LED_DEGREES/2 - veh.e_a) / step)
+    elif veh.e_a >= 360 - LED_DEGREES/2:
+        return round((-(veh.e_a - 360) + LED_DEGREES/2) / step)
+        #return round((veh.e_a - LED_DEGREES)/ step)
+        return round(((360 - veh.e_a) + LED_DEGREES/2) / step)
+    else:
+        return None
+
+def simple_led_pattern(vehicles):
+    def change_light(add_val, f_pixels, led_pos, led_ord):
+        if add_val <= 0:
+            return f_pixels
+        elif add_val >= 1:
+            if led_ord == 0:
+                f_pixels[led_pos] = 1
+                return f_pixels
+            else:
+                try:
+                    f_pixels[led_pos + led_ord] += 1
+                except IndexError:
+                    pass
+                try:
+                    f_pixels[led_pos - led_ord] += 1
+                except IndexError:
+                    pass
+                return f_pixels
+        else:
+            if led_ord == 0:
+                f_pixels[led_pos] += add_val
+                return f_pixels
+            else:
+                try:
+                    f_pixels[led_pos + led_ord] += add_val
+                except IndexError:
+                    pass
+                try:
+                    f_pixels[led_pos - led_ord] += add_val
+                except IndexError:
+                    pass
+                return f_pixels
+    
+    # Noof pixels representing back of car on every side
+    b_pixels = 10
+    f_pixels = PIXEL_COUNT - b_pixels * 2
+    pix_vec = [0] * f_pixels
+    threshold = 200
+    step = LED_DEGREES / f_pixels
+    led_pref = [
+                [200, 60],
+                [150, 45],
+                [100, 30],
+                [60, 20],
+                [30, 10],
+                [18, 0]
+                ]
+    
+    vehicles.pop(0)
+    for i in vehicles:
+        if i.e_d <= threshold:
+            led_c = led_position(i, step, f_pixels)
+            if led_c != None:
+                bright_list = []
+                for j in led_pref:
+                    bright_list.append((j[0] - i.e_d) / (j[0] - j[1]))
+                print(bright_list)
+                for j in range(len(bright_list)):
+                    """
+                    bright_list j = 
+                    pix vec = Total vectors of pixels used for LED strip
+                    ledc = the central LED position
+                    j = diversion from ledc
+                    """
+                    pix_vec = change_light(bright_list[j], pix_vec, led_c, j)
+                    
+    
+    back_vec = [0] * b_pixels
+    pix_vec.extend(back_vec)
+    tot_vec = [0] * b_pixels
+    tot_vec.extend(pix_vec)
+    return tot_vec
+                
+
+def show_LEDs(pixels, led_vector):
+    for i in range(len(led_vector)):
+        pixels.set_pixel_rgb(i, int(led_vector[i] * 255), 0, 0)
+    # pixels.clear()
+    pixels.show()
+
+if __name__ == "__main__":
+    # Clear all the pixels to turn them off.
+    pixels.clear()
+    pixels.show()  # Make sure to call show() after changing any pixels!
+
+
+
+
+
+while True:
+    data, addr = sock.recvfrom(300) # buffer size is 1024 bytes
+    vehicles = format_string(str(data))
+    vehicles[1].x
+    vehicles = transposing_vehicles(vehicles)
+    vehicles = dist_to_ego(vehicles)
+    vehicles = angle_to_ego(vehicles)
+    for i in vehicles:
+        i.status() 
+    led_vector = simple_led_pattern(vehicles)
+    print(led_vector)
+    show_LEDs(pixels, led_vector)
+    
+    
+
+    #print("received message:", data)
+    
